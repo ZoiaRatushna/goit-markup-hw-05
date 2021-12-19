502642423103fa047485e2cb6ecfb780879a0eca
ZoiaRatushna <93883758+ZoiaRatushna@users.noreply.github.com>
2021-12-19 17:11:25 +0200

    hw_5_modal_w

--- js/modal.js
+++ js/modal.js
@@ -4,11 +4,16 @@
       closeModalBtn: document.querySelector('.data-modal-close'),
       modal: document.querySelector('.modal'),
     };
+
+    if (refs.modal) {
+      refs.modal.style.display = 'none';
+    }
   
     refs.openModalBtn.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
   
     function toggleModal() {
+      refs.modal.style.display = 'flex';
       refs.modal.classList.toggle('is-hidden');
     }
   })();
